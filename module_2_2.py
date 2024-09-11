615320fcb13cd8fdb5773dfab757f243c56a51cf
Felicialite19 <felicialite@gmail.com>
2024-09-11 17:44:43 +0400

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -4,7 +4,7 @@ third=int(input('Введите третье число: '))
 
 if first == second == third :
     print(3)
-if first == second or first == third or second == third :
+elif first == second or first == third or second == third :
     print(2)
 else:
     print(0)
